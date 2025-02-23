29a463e33f05eeeae1bc12b491585ccee29a05a7
Agboola Akorede <akoredeagboola23@gmail.com>
2025-02-23 06:39:41 +0100

    feat: event driven

---
+++ src/events/constants/event-/event-constants.ts
@@ -0,0 +1,5 @@
+export const USER_REGISTERED = 'user.registered';
+export const USER_PASSWORD_RESET = 'user.passwordReset';
+export const ORDER_PLACED = 'order.placed';
+export const ORDER_DELIVERED = 'order.delivered';
+// Add more events as needed
