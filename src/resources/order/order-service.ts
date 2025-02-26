f44c59e09fe18d645a455a6bafc41279a2273ce8
Agboola Akorede <akoredeagboola23@gmail.com>
2025-02-26 07:22:12 +0100

    chore : update file name

--- src/resources/order/order-service.ts
+++ src/resources/order/order-service.ts
@@ -2,8 +2,6 @@ import { Request, Response } from 'express';
 import UserModel from '../user/user-model';
 import OrderModel from '../order/order-model';
 import { IOrderPaginatedResponse } from '../../types/index';
-import { config } from '../../config/index';
-import MenuModel from '../menu/menu-model';
 import {
     IOrder,
     DeliveryInfo,
@@ -21,95 +19,22 @@ import {
     orderStatusUpdateEmail,
     orderCancellationEmail,
 } from '../order/order-email-template';
+import { ResourceNotFound, BadRequest } from '../../middlewares/index';
+
 import {
-    ResourceNotFound,
-    BadRequest,
-    Unauthorized,
-} from '../../middlewares/index';
+    orderData,
+    validateUser,
+    checkOrderOwnership,
+    calculateOrderAmounts,
+} from '../order/order-helper';
 
 export class OrderService {
     private order = OrderModel;
     private user = UserModel;
-    private menu = MenuModel;
-
-    private async validateUser(userId: string) {
-        const user = await this.user.findById(userId);
-        if (!user) {
-            throw new ResourceNotFound('User not found');
-        }
-        return user;
-    }
-    private async checkOrderOwnership(
-        orderId: string,
-        restaurantId: string,
-    ): Promise<void> {
-        const order = await this.order
-            .findOne({
-                _id: orderId,
-                restaurantId: restaurantId,
-            })
-            .lean();
-
-        if (!order) {
-            throw new Unauthorized(
-                'Order not found or does not belong to this restaurant.',
-            );
-        }
-    }
-    private async calculateOrderAmounts(
-        items: { menuId: string; quantity: number }[],
-    ) {
-        const itemsWithPrices = await Promise.all(
-            items.map(async (item) => {
-                const menuItem = await this.menu.findById(item.menuId);
-                if (!menuItem) {
-                    throw new ResourceNotFound(
-                        `Menu item with ID ${item.menuId} not found`,
-                    );
-                }
-                return {
-                    ...item,
-                    price: menuItem.price,
-                    name: menuItem.name,
-                };
-            }),
-        );
-
-        const subtotal = itemsWithPrices.reduce(
-            (acc, item) => acc + item.quantity * item.price,
-            0,
-        );
-
-        const taxRate = parseFloat(config.TAX_RATE);
-        const delivery_fee = parseFloat(config.DELIVERY_FEE);
-        const tax = subtotal * taxRate;
-        const total_price = subtotal + tax + delivery_fee;
-
-        return {
-            itemsWithPrices,
-            roundedSubtotal: Math.round(subtotal * 100) / 100,
-            roundedTax: Math.round(tax * 100) / 100,
-            roundedTotalPrice: Math.round(total_price * 100) / 100,
-            delivery_fee,
-        };
-    }
-    private sanitizeOrder(order: IOrder): Partial<IOrder> {
-        return {
-            _id: order._id,
-            order_number: order.order_number,
-            status: order.status,
-            total_price: order.total_price,
-            userId: order.userId,
-            restaurantId: order.restaurantId,
-            items: order.items,
-            subtotal: order.subtotal,
-            tax: order.tax,
-            delivery_fee: order.delivery_fee,
-            delivery_info: order.delivery_info,
-            createdAt: order.createdAt,
-            updatedAt: order.updatedAt,
-        };
-    }
+    private orderData = orderData;
+    private validateUser = validateUser;
+    private checkOrderOwnership = checkOrderOwnership;
+    private calculateOrderAmounts = calculateOrderAmounts;
 
     public async placeOrder(
         userId: string,
@@ -119,7 +44,7 @@ export class OrderService {
             delivery_address: string;
         },
     ): Promise<Partial<IOrder>> {
-        const user = await this.validateUser(userId);
+        await this.validateUser(userId);
 
         const order_number = await generateOrderId();
 
@@ -147,7 +72,7 @@ export class OrderService {
             total_price: roundedTotalPrice,
         });
 
-        return this.sanitizeOrder(newOrder);
+        return this.orderData(newOrder);
     }
 
     public async updateOrderStatus(
@@ -167,7 +92,7 @@ export class OrderService {
             const emailOptions = orderStatusUpdateEmail(user, updatedOrder);
             await EmailQueueService.addEmailToQueue(emailOptions);
         }
-        return this.sanitizeOrder(updatedOrder);
+        return this.orderData(updatedOrder);
     }
 
     public async cancelOrder(
@@ -194,7 +119,7 @@ export class OrderService {
             await EmailQueueService.addEmailToQueue(emailOptions);
         }
 
-        return this.sanitizeOrder(updatedOrder!);
+        return this.orderData(updatedOrder!);
     }
 
     public async getOrderById(
@@ -210,7 +135,7 @@ export class OrderService {
                 if (!order) {
                     throw new ResourceNotFound('Order not found');
                 }
-                return this.sanitizeOrder(order);
+                return this.orderData(order);
             },
             CACHE_TTL.FIVE_MINUTES,
         );
